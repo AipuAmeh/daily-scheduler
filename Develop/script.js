8b4dde1a33effa102409e0066999fd5fc75c5d24
AipuAmeh <aipuameh.16@gmail.com>
2023-04-18 23:29:59 -0400

    did one half of setting user's input to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,7 @@ const three = $("hour-3").data("time");
 const four = $("hour-4").data("time");
 const five = $("hour-5").data("time");
 const timeBlock = $(".time-block");
-
+var saveButton = $(".saveBtn");
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -22,7 +22,11 @@ $(function () {
 //   // function? How can DOM traversal be used to get the "hour-x" id of the
 //   // time-block containing the button that was clicked? How might the id be
 //   // useful when saving the description in local storage?
-//   //
+saveButton.on("click", function() {
+var userInput = $("div").children("div.time-block").children("id");
+ localStorage.setItem("id","userInput");
+  console.log(userInput);
+})
 //   // TODO: Add code to apply the past, present, or future class to each time
 //   // block by comparing the id to the current hour. HINTS: How can the id
 //   // attribute of each time-block be used to conditionally add or remove the
